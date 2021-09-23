c44639179c4564e0aac967c8c79e0cd1f1325d89
jatacumas <jatacumas@unal.edu.co>
2021-09-22 21:33:13 -0500

    reto cuatro del ciclo java

---
+++ Reto_cuatro.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Reto_cuatro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Reto_cuatro
+{
+    public static void main(String [] args){
+    
+    Usuario usj= new Usuario ();
+    usj.addTelefono("333333");
+    usj.addTelefono("111111");
+    usj.addTelefono("444444");
+    usj.addTelefono("222222");
+    usj.mostrarTelefonos();
+    
+      Usuari usi= new Usuari ();
+    usj.addTelefono("333333");
+    usj.addTelefono("111111");
+    usj.addTelefono("444444");
+    usj.addTelefono("222222");
+    usj.mostrarTelefonos();
+    }
+}
